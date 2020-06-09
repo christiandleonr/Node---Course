933c72f415de9a9030356aea2725663f42afee50
Christian Ramirez <christiandleonr@gmail.com>
2020-06-09 12:49:16 -0600

    Section 3 - Lesson 12

--- Course/section-3/app.js
+++ Course/section-3/app.js
@@ -8,10 +8,22 @@
 // 3. Export getNotes function
 // 4. From app.js, load in and call the function printing message to console
 
-const validator = require('validator')
-const getNotes = require('./notes')
+// const validator = require('validator')
+// const getNotes = require('./notes')
 
-const note = getNotes()
-console.log(note)
+// const note = getNotes()
+// console.log(note)
 
-console.log(validator.isURL('chiesa'))
+// console.log(validator.isURL('chiesa'))
+
+// Challenge: Use the chalk library in your project
+// 1. Install chalk
+// 2. Load chalk into app.js
+// 3. Use it to print the String "Success!" to the console in green
+// 4. Test your work
+
+const chalk = require('chalk')
+const greenMessage = chalk.green.underline('Success!')
+const yellowMessage = chalk.yellow.underline('Warning!')
+const redMessage = chalk.red.underline('Error!')
+console.log(greenMessage, yellowMessage, redMessage)
