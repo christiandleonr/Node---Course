8d4f2b300ab125e9cf7756359974777d2b9892ad
Christian Ramirez <christiandleonr@gmail.com>
2020-06-09 12:10:32 -0600

    Section 3 - Lesson 10 Agregando cambios en app.js

--- Course/section-3/app.js
+++ Course/section-3/app.js
@@ -1,4 +1,13 @@
-const fs = require('fs')
+// const add = require('./utils')
+// const sum = add(5, -1)
+// console.log(sum)
 
-fs.writeFileSync('notes.txt', 'My name is Christian')
-fs.appendFileSync('notes.txt', '\nI live in Culiacan')
+// Challenge: Define and use a fucntion in a new file
+// 1. Create a new file called notes.js
+// 2. Create getNotes function that returns "Your notes..."
+// 3. Export getNotes function
+// 4. From app.js, load in and call the function printing message to console
+
+const getNotes = require('./notes')
+const note = getNotes()
+console.log(note)
