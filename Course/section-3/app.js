6199bc4be92192e661bc5a9b824a3e21e16a79a9
Christian Ramirez <christiandleonr@gmail.com>
2020-06-09 12:31:40 -0600

    Section 3 - Lesson 11

--- Course/section-3/app.js
+++ Course/section-3/app.js
@@ -8,6 +8,10 @@
 // 3. Export getNotes function
 // 4. From app.js, load in and call the function printing message to console
 
+const validator = require('validator')
 const getNotes = require('./notes')
+
 const note = getNotes()
 console.log(note)
+
+console.log(validator.isURL('chiesa'))
