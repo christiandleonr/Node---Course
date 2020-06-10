1e5512b36c5b1a4e980d4dec80a54f0e34d2222d
Christian Ramirez <christiandleonr@gmail.com>
2020-06-10 14:44:19 -0600

    Section 4 - Lesson 18

---
+++ Course/Section-4/playground/1-json.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday'
+// }
+
+// const bookJSON = JSON.stringify(book)
+// console.log(bookJSON)
+
+// const parsedData = JSON.parse(bookJSON)
+// console.log(parsedData.author)
+
+// fs.writeFileSync('1-json.json', bookJSON)
+
+// const dataBuffer = fs.readFileSync('1-json.json')
+// const dataJSON = dataBuffer.toString()
+// const data = JSON.parse(dataJSON)
+// console.log(data.title)
+
+// Challenge: Work with JSON data
+// 1. Load and parse the JSON data
+// 2. Change the name and age property using your info
+// 3. Stringify the changed object and overwrite the original data
+// 4. Test your work by viewing data in the JSON file
+
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dataBuffer.toString()
+const data = JSON.parse(dataJSON)
+
+data.name = 'Christian Ramirez de Leon'
+data.age = '22'
+
+const userJSON = JSON.stringify(data)
+fs.writeFileSync('1-json.json', userJSON)
+
+
