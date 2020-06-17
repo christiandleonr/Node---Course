13de69478322d3ad781dc6020eef8e60a4e7a48d
Christian Ramirez <christiandleonr@gmail.com>
2020-06-17 16:33:01 -0600

    Section 6 - Lesson 31 updated

--- Course/Section-6/app.js
+++ Course/Section-6/app.js
@@ -3,6 +3,7 @@ const request = require('request')
 const url = 'http://api.weatherstack.com/current?access_key=663ecca95d1ba31abcaa548761fb4a91&query=25.0,%20-107.5'
 
 request({ url: url }, (error, response) => {
+    // This create a object that we can use to access to the information
     const data = JSON.parse(response.body)
 
     console.log(data.current.temperature)
