8b45275af93a21cf656c419226e27fea324a27f2
Christian Ramirez <christiandleonr@gmail.com>
2020-07-07 14:29:31 -0600

    Section 7 - Lesson 48

--- Course/Section-7/web-server/src/app.js
+++ Course/Section-7/web-server/src/app.js
@@ -4,7 +4,34 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index',{
+        title: 'Weather App',
+        name: 'Christian Ramirez'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Christian Ramirez'
+    })
+})
+
+// Goal: Create a template for help page
+// 1. Setup a help template to render a help message to the screen
+// 2. Setup the help route and render the template with an example message
+// 3. Visit the route in the browser and see your help message print
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help page',
+        helpMessage: "I'm trying to be good"
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
